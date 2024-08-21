b44728a95c3c0d02ceb120ab242ea0ab36cdb43c
Kevin <69663625+veilkev@users.noreply.github.com>
2024-08-20 23:08:46 -0500

    removed staged bash

--- deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Pass the argument to the remote deploy.sh script
-ssh -p 21098 veilkro@digitajoe.com "/home/veilkro/digitajoe.com/deploy.sh $1"
--- log.sh
+++
@@ -1,2 +0,0 @@
-ssh -p 21098 veilkro@digitajoe.com "/home/veilkro/digitajoe.com/log.sh"
-
